fa9b91ec339867c9847bfb26c994adda7e88dbc6
Helen Calderon <helencalderon@gmail.com>
2020-02-24 13:41:02 +0000

    Updated images throughout

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,26 @@ $("#footer-nav a").each(function() {
         $(this).addClass("active");
     }
 });
+
+
+function showImages(el) {
+    var windowHeight = jQuery( window ).height();
+    $(el).each(function(){
+        var thisPos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+        if (topOfWindow + windowHeight - 200 > thisPos ) {
+            $(this).addClass("fade-in");
+            }
+        });
+    }
+
+    // if the image in the window of browser when the page is loaded, show that image
+    $(document).ready(function(){
+            showImages('.post img');
+    });
+
+    // if the image in the window of browser when scrolling the page, show that image
+    $(window).scroll(function() {
+            showImages('.post img');
+    });
